1354136e899e2f21243d9dd80ba7075570c962d4
Usman Jamil <usman.jamil@live.co.uk>
2016-01-28 14:18:01 +0000

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hackerNewsHomepageApp
+ * @description
+ * # hackerNewsHomepageApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hackerNewsHomepageApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hackerNewsHomepageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hackerNewsHomepageApp
+ */
+angular.module('hackerNewsHomepageApp')
+  .controller('MainCtrl', function () {
+
+  });
