eeb901d5fa115511e6672b2b751a6bbb7996ea59
Usman Jamil <usman.jamil@live.co.uk>
2016-01-28 21:43:07 +0000

    receiving top 30 news but in a shuffled order

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,36 @@
  * Controller of the hackerNewsHomepageApp
  */
 angular.module('hackerNewsHomepageApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', ['$scope', 'New', 'Story', function ($scope, New, Story) {
 
-  });
+    $scope.stories = [];
+    $scope.topStories = 0;
+    $scope.loaded = false;
+
+    $scope.getTop = function() {
+      New.get()
+        .then(function(response) {
+          $scope.topStories = response.data;
+          console.log(response.data)
+          $scope.getStories($scope.topStories);
+      });
+    };
+
+    $scope.getStories = function(topStoryId) {
+      var i;
+      for (i = 0; i < 31; i++) {
+        Story.get(topStoryId[i])
+        .then(function(response) {
+          $scope.stories.push(response.data);
+          $scope.loaded = true;
+          console.log($scope.stories[1])
+          console.log($scope.stories[5])
+        });
+      }
+    };
+
+    angular.element(document).ready(function () {
+      $scope.getTop();
+    });
+
+  }]);
